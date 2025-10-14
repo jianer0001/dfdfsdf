db52f6b058e5dc6470a8316a78792d7cb0a25127
jian <821330378@qq.com>
2025-10-14 13:11:51 +0800

    初始化项目

--- functions/api/parse1.js
+++ functions/api/parse1.js
@@ -10,7 +10,7 @@ export async function onRequestPost(context) {
     let jian = 'jian';
     
     try {
-        result0 = await context.env.SERVICE.fetch('/test/jian', { method: 'GET' });
+        result0 = await context.env.SERVICE.fetch('https://guanhen.821330378.workers.dev/test/jian', { method: 'GET' });
         // 检查响应状态
         if (!result0.ok) {
             console.warn(`SERVICE fetch failed with status ${result0.status}`);
@@ -32,7 +32,7 @@ export async function onRequestPost(context) {
             }),
             errorStr1 : error.toString(),
 
-            service: JSON.stringify(context.env.SERVICE)
+            service: typeof context.env.SERVICE
         };
     }
 
