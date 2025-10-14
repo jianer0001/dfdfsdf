8b60167f63a3f616e29ed084950562392fbceb0f
jian <821330378@qq.com>
2025-10-14 10:54:09 +0800

    初始化项目

--- src/api/workerApi.js
+++ src/api/workerApi.js
@@ -1,7 +1,7 @@
 import service from './http';
 
 export async function parseFile(fileBuffer) {
-  const response = await service.post('/parse', fileBuffer, {
+  const response = await service.post('/parse1', fileBuffer, {
     headers: { 'Content-Type': 'application/octet-stream' }
   });
   return response.data;
