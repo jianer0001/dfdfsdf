75597d52ed01eb983b2aa7b4c147e8e54a6f9c74
jian <821330378@qq.com>
2025-10-15 09:42:19 +0800

    初始化项目

--- functions/gh/file/parse1.js
+++ functions/gh/file/parse1.js
@@ -1,4 +1,4 @@
 export async function onRequestPost(context) {
     context.request.url = 'https://guanhen.821330378.workers.dev'
-    return context.env.JIAN.fetch(context.request, {method: 'GET'});
+    return context.env.JIAN.fetch(context.request.url, {method: 'GET'});
 }
