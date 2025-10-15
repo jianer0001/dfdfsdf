ada60aa1269de9a2513db68d16898f3fb40509d9
jian <821330378@qq.com>
2025-10-15 09:39:18 +0800

    初始化项目

--- functions/gh/file/parse1.js
+++ functions/gh/file/parse1.js
@@ -1,4 +1,4 @@
 export async function onRequestPost(context) {
-    context.request.url = 'https://guanhen.821330378.workers.dev/file/parse-excel'
-    return context.env.JIAN.fetch(context.request);
+    context.request.url = 'https://guanhen.821330378.workers.dev'
+    return context.env.JIAN.fetch(context.request, {method: 'GET'});
 }
